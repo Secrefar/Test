338d5096b853111d8ac945cb1dc3f3e7103cb288
Secrefar <k.nazarova90@gmail.com>
2022-01-29 20:47:00 +0300

    added solution

--- Program.cs
+++ Program.cs
@@ -1,2 +1,27 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿string[] array = {"8ddkk", "wd5", "=7bw", "q", "86+", "hs10", "#4=", "g5", "-="};
+
+int sizeNewArray = 0;
+
+for (int i = 0; i < array.Length; i++)
+{
+    if (array[i].Length <= 3) sizeNewArray++;
+}
+
+string[] newarray = new string[sizeNewArray];
+
+int a = 0;
+int n = 0;
+
+while (a < array.Length)
+{
+    if (array[a].Length <= 3) 
+
+    {   
+        newarray[n] = array[a];
+        Console.WriteLine(newarray[n]);
+        a++;
+        n++;
+    }
+
+    else a++;
+}
