44dbb6f6d2d881357f1927f55285eab42838fe45
Secrefar <k.nazarova90@gmail.com>
2022-01-30 13:51:29 +0300

    added flow chart

--- Program.cs
+++ Program.cs
@@ -1,20 +1,20 @@
 ﻿string[] array = {"8ddkk", "wd5", "=7bw", "q", "86+", "hs10", "#4=", "g5", "-="};
 
 int sizeNewArray = 0;
+int a = 0;
+int n = 0;
+int b = 3;
 
 for (int i = 0; i < array.Length; i++)
 {
-    if (array[i].Length <= 3) sizeNewArray++;
+    if (array[i].Length <= b) sizeNewArray++;
 }
 
 string[] newarray = new string[sizeNewArray];
 
-int a = 0;
-int n = 0;
-
 while (a < array.Length)
 {
-    if (array[a].Length <= 3) 
+    if (array[a].Length <= b) 
 
     {   
         newarray[n] = array[a];
